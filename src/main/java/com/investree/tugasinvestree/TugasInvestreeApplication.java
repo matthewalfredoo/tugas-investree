0aeb4298f4d7340a15f7dc8950b23e2e61616ebc
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-06-05 19:20:31 +0700

    First Commit

---
+++ src/main/java/com/investree/tugasinvestree/TugasInvestreeApplication.java
@@ -0,0 +1,13 @@
+package com.investree.tugasinvestree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasInvestreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TugasInvestreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/investree/tugasinvestree/TugasInvestreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investree.tugasinvestree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasInvestreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

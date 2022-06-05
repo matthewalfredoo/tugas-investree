2a283c43b71c52601d20163c1aceb76ad3d3aadb
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-06-05 21:10:53 +0700

    soal-tiga

---
+++ src/main/java/com/investree/tugasinvestree/view/impl/TransaksiPaymentImple.java
@@ -0,0 +1,50 @@
+package com.investree.tugasinvestree.view.impl;
+
+import com.investree.tugasinvestree.model.Transaksi;
+import com.investree.tugasinvestree.repository.TransaksiRepository;
+import com.investree.tugasinvestree.view.TransaksiService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TransaksiPaymentImple implements TransaksiService {
+
+    @Autowired
+    public TransaksiRepository transaksiRepository;
+
+    @Override
+    public Map save(Transaksi transaksi) {
+        Map map = new HashMap();
+        try{
+            Transaksi insertTransaksi = transaksiRepository.save(transaksi);
+            map.put("data", insertTransaksi);
+            map.put("code", "200");
+            map.put("status", "sukses");
+            return map;
+        } catch (Exception e){
+            map.put("code", "500");
+            map.put("status", "gagal");
+            return map;
+        }
+    }
+
+    @Override
+    public Map updateStatus(Transaksi transaksi) {
+        Map map = new HashMap();
+        try{
+            Transaksi updateTransaksi = transaksiRepository.getbyID(transaksi.getId());
+            // updateTransaksi.setStatus(transaksi.getStatus());
+            updateTransaksi.setStatus("Lunas");
+            Transaksi savedUpdatedTransaksi = transaksiRepository.save(updateTransaksi);
+            map.put("data", savedUpdatedTransaksi);
+            map.put("code", "200");
+            map.put("status", "sukses");
+            return map;
+        } catch (Exception e){
+            map.put("code", "500");
+            map.put("status", "gagal");
+            return map;
+        }
+    }
+}
